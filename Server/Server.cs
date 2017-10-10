43c37dabdf4f92780b851981bcfe6f47a1c0f976
Unknown <lexach16@gmail.com>
2017-10-10 12:45:19 +0300

    Init

---
+++ Server/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    class Server
+    {
+        Socket socket;
+
+
+        public Server(int port)
+        {
+            IPHostEntry iphost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = iphost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 55000);
+
+            socket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipEndPoint);
+        }
+
+        public void StartServer()
+        {
+            if (socket == null)
+                throw new NullReferenceException();
+            socket.Listen(10);
+            int connections = 0;
+            while (connections < 10)
+            {
+                Socket handler = socket.Accept();
+                connections++;
+                Thread thread = new Thread(new ParameterizedThreadStart(AcceptConnection));
+                thread.Start(handler);
+            }
+            socket.Close();
+        }
+
+        void AcceptConnection(object h)
+        {
+            Socket handler = (Socket)h;
+            Console.WriteLine("Connection accepted");
+           
+            handler.Send(Encoding.UTF8.GetBytes("Connection accepted"));
+            Thread.Sleep(10000);
+            handler.Send(Encoding.UTF8.GetBytes("Connection closed"));
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+    }
+}
